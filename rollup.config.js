2a4c48f4379d47fb1c89e7c45a8f238c025f5153
Thiago Paiva <10606829+paiva-thiago@users.noreply.github.com>
2019-12-17 15:49:01 -0300

    Testing aside modules before routing

--- rollup.config.js
+++ rollup.config.js
@@ -63,6 +63,7 @@ export default {
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
 		production && terser()
+		
 	],
 	watch: {
 		clearScreen: false
--- src/App.svelte
+++ src/App.svelte
@@ -3,50 +3,28 @@
 	 import BlogHeader 		from './BlogHeader.svelte'
 	 import Blog            from './Blog.svelte'
 	 import BlogFooter 		from './BlogFooter.svelte'
+	 import dataRouted     from './LejaRouting.js'
 	 
-
 	 async function getData(){
-		let ret={}
-		const existReq = location.href.indexOf('?')>0;		
-		const res = await fetch('./data/blog-data.json');
-		const text = await res.text()	
-		const getTitle = (cont)=>(cont.split('/')[0]==='pages'?cont.split('/')[1].replace(/_/g,' '):cont.split('-')[1].replace(/_/g,' '))	
-		console.log('texto = '+text);		
-		if (res.ok) {
-			ret= JSON.parse(text)
-			if(existReq){
-				ret.posts=[]
-				ret.singleText=true;
-				const content = location.href.split('?')[1]
-				const resCont = await fetch(`./data/${content}.md`);
-				const txtCont = await resCont.text();
-				if(resCont.ok){
-					ret.titleText = getTitle(content)
-					ret.text = txtCont;
-				}else{
-					throw new Error(text)
-					return ret
-				}
-			}
-		} else {			
-			throw new Error(text)
-			return ret
-		}		
-		console.log(ret);
-		return ret;
-	 }
-	 let blogData = getData();
+		let data={}
+		try{
+			data =  await dataRouted()
+			console.log('testing...')
+		}catch(e){
+			throw new Error('Falha')
+		}
+		return data
+	 }	 
+	 let blogData = getData()
 	 
 </script>
 <body>
 	{#await blogData}
 		<ContentLoader>
-		  <rect x="0" y="0" rx="3" ry="3" width="250" height="10" />
-		  <rect x="20" y="20" rx="3" ry="3" width="220" height="10" />
-		  <rect x="20" y="40" rx="3" ry="3" width="170" height="10" />
-		  <rect x="0" y="60" rx="3" ry="3" width="250" height="10" />
-		  <rect x="20" y="80" rx="3" ry="3" width="200" height="10" />
-		  <rect x="20" y="100" rx="3" ry="3" width="80" height="10" />
+		  <rect x="0" y="80" rx="3" ry="3" width="350" height="6" /> 
+		  <rect x="0" y="100" rx="3" ry="3" width="380" height="6" /> 
+		  <rect x="0" y="120" rx="3" ry="3" width="201" height="6" /> 
+		  <rect x="6" y="7" rx="0" ry="0" width="321" height="59" />
 		</ContentLoader>
 	{:then retorno}
 		<BlogHeader retorno={retorno}/>
